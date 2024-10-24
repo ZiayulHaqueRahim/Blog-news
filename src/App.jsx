bad7491b9ceb2a48c0f6043a1604256f712d2eee
Zia <ziair3812@gmail.com>
2024-10-25 02:23:51 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Blogs from './components/Blogs/blogs'
+import Header from './components/Header/header'
+
+function App() {
+  
+
+  return (
+    <>
+     <Header></Header>
+      <Blogs></Blogs>
+
+
+    </>
+  )
+}
+
+export default App
